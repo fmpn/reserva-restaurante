2a9ea66739f7cdb43e4aa8cd8013b0fa666774b7
guilhermepaiva <guilhermepaiva9@gmail.com>
2013-12-13 21:02:15 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReservaRestaurante::Application.config.secret_key_base = '4571b6218bf413e5974ab3038dd2c0399641fbf4a7692478bcec2788100f77c65764e4c4317792f960ca4098c26e0a6171b09ee8cc5e68a70474b69f0874d210'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReservaRestaurante::Application.config.session_store :cookie_store, key: '_reserva-restaurante_session'
